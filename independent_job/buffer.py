1c872cc2e1be8b41a4919b7240ef96899a81a24d
hongseong0314 <mi70107010@gmail.com>
2023-09-26 06:53:58 +0000

    .

--- independent_job/buffer.py
+++ independent_job/buffer.py
@@ -1,93 +1,59 @@
 import numpy as np
 import torch
+import gc
+from dataclasses import dataclass
 
-class ReplayBuffer():
+@dataclass
+class BufferState:
+    machine_feature: torch.Tensor = None
+    task_feature: torch.Tensor = None
+    D_TM: torch.Tensor = None
+    ninf_mask: torch.Tensor = None
+    logpa : torch.Tensor = None
+
+class EpisodeBuffer():
     """
     버퍼 기본
     """
-    def __init__(self, 
-                 buffer_limit=10000, 
-                 batch_size=64):
-        self.s_mem = np.empty(shape=(buffer_limit), dtype=np.ndarray)
-        self.s_prime_mem = np.empty(shape=(buffer_limit), dtype=np.ndarray)
+    def __init__(self,
+                 buffer_limit=10000,
+                 batch_size=12):
+        self.s_mem = np.empty(shape=(batch_size, buffer_limit), dtype=np.ndarray)
         self.max_size = buffer_limit
         self.batch_size = batch_size
+        self.trajectory_pointer = 0
         self._idx = 0
-        self.pre_idx = 0
         self.size = 0
-    
-    def put(self, prime_state, state):
-        self.s_mem[self._idx] = state
-        self.s_prime_mem[self._idx] = prime_state
-        self.pre_idx = self._idx
+        self.trajectory_max_T, self.G_ts = [], []
+
+    def trajectory_up(self, G_t):
+        self.trajectory_pointer += 1
+        self.trajectory_max_T.append(self._idx)
+        self.G_ts.append(G_t)
+        self._idx = 0
+        self.size = 0
+
+    def put(self, state):
+        self.s_mem[self.trajectory_pointer, self._idx] = state
         self._idx += 1
         self._idx = self._idx % self.max_size
 
         self.size += 1
         self.size = min(self.size, self.max_size)
 
-    def sample(self, batch_size=None):
-        if batch_size == None:
-            batch_size = self.batch_size
-
-        idxs = np.random.choice(
-            self.size, batch_size, replace=False)
-
-        batch_s = self.s_mem[idxs]
-        batch_s_prim = self.s_prime_mem[idxs]
-
-        machine_feature, task_feature, D_TM, ninf_mask, available_task_idx, reward, action, done = \
-        [],[],[],[],[],[],[],[]
-        machine_feature_s_prime, task_feature_s_prime, D_TM_s_prime,\
-        ninf_mask_s_prime, available_task_idx_s_prime, reward_s_prime, action_s_prime, done_s_prime = \
-        [],[],[],[],[],[],[],[]
-        
-        for s, s_prime in zip(batch_s, batch_s_prim):
-                machine_feature.append(s.machine_feature); machine_feature_s_prime.append(s_prime.machine_feature)
-                task_feature.append(s.task_feature); task_feature_s_prime.append(s_prime.task_feature)
-                D_TM.append(s.D_TM); D_TM_s_prime.append(s_prime.D_TM)
-                
-                
-                ninf_mask.append(s.ninf_mask); ninf_mask_s_prime.append(s_prime.ninf_mask)
-                #available_task_idx.append(s.available_task_idx); available_task_idx_s_prime.append(s_prime.available_task_idx)
-                
-                reward.append(s.reward); reward_s_prime.append(s_prime.reward)
-                action.append(s.action); action_s_prime.append(s_prime.action)
-                done.append(s.done); done_s_prime.append(s_prime.done)
-
-        machine_feature = torch.stack(machine_feature).squeeze(dim=1)
-        task_feature = torch.stack(task_feature).squeeze(dim=1)
-        D_TM = torch.stack(D_TM).squeeze(dim=1)
-        ninf_mask = torch.stack(ninf_mask).squeeze(dim=1)
-
-        reward = torch.tensor(reward)
-        action = torch.tensor(action)
-        done = torch.tensor(done)
-
-        machine_feature_s_prime = torch.stack(machine_feature_s_prime).squeeze(dim=1)
-        task_feature_s_prime = torch.stack(task_feature_s_prime).squeeze(dim=1)
-        D_TM_s_prime = torch.stack(D_TM_s_prime).squeeze(dim=1)
-        ninf_mask_s_prime = torch.stack(ninf_mask_s_prime).squeeze(dim=1)
-
-        reward_s_prime = torch.tensor(reward_s_prime)
-        action_s_prime = torch.tensor(action_s_prime)
-        done_s_prime = torch.tensor(done_s_prime)
-
-        experiences = machine_feature, \
-                        task_feature, \
-                        D_TM, \
-                        ninf_mask, \
-                        reward, \
-                        action, \
-                        done, \
-                        machine_feature_s_prime, \
-                        task_feature_s_prime, \
-                        D_TM_s_prime, \
-                        ninf_mask_s_prime, \
-                        reward_s_prime, \
-                        action_s_prime, \
-                        done_s_prime
-        return experiences
+    def get_trajectory(self, batch_size=None):
+        return np.array([self.s_mem[i, :self.trajectory_max_T[i]] \
+                         for i in range(self.trajectory_pointer)]), self.G_ts
 
     def __len__(self):
         return self.size
+
+    def reset(self):
+        self.s_mem = np.empty(shape=(self.batch_size, self.max_size), dtype=np.ndarray)
+        self.trajectory_pointer = 0
+        self._idx = 0
+        self.size = 0
+
+        self.trajectory_max_T, self.G_ts = [], []
+        gc.collect()
+        pass
--- independent_job/matrix/agent.py
+++ independent_job/matrix/agent.py
@@ -1,9 +1,11 @@
 import torch
-
+import numpy as np
 from torch.optim import Adam as Optimizer
 from torch.optim.lr_scheduler import MultiStepLR, LambdaLR, CosineAnnealingLR
 from independent_job.matrix.model import CloudMatrixModel, CloudMatrixModel_one_pose
 from independent_job.matrix.utill import CosineAnnealingWarmUpRestarts
+import copy
+from independent_job.buffer import EpisodeBuffer, BufferState
 
 class BGCD():
     def __init__(self, cfg):
@@ -190,3 +192,151 @@ class BGCM():
 
         self.logpa_sum, self.G_ts, self.entropies = [], [], []
         return loss_mean.detach().cpu().numpy()
+
+class BGCDPPO():
+    def __init__(self, cfg):
+        self.device = cfg.model_params['device']
+        self.model = CloudMatrixModel_one_pose(**cfg.model_params).to(self.device)
+        self.optimizer = Optimizer(self.model.parameters(), **cfg.optimizer_params['optimizer'])
+        self.scheduler = LambdaLR(self.optimizer, lr_lambda=lambda epoch: 0.995 ** epoch)
+
+        self.save_path = cfg.model_params['save_path']
+        self.load_path = cfg.model_params['load_path']
+        self.skip = cfg.model_params['skip']
+        self.machine_num = cfg.machines_number
+
+        self.policy_loss_weight = cfg.model_params['policy_loss_weight']
+        self.entropy_loss_weight = cfg.model_params['entropy_loss_weight']
+        self.policy_clip_range = 0.2
+        if self.load_path:
+            print(f"load weight : {self.load_path}")
+            self.model.load_state_dict(torch.load(cfg.model_params['load_path'],
+                                                  map_location=self.device))
+            self.model.eval()
+
+        self.buffer = EpisodeBuffer()
+        self.buffer_state = BufferState()
+
+    def model_save(self, save_path=None):
+        if save_path == None:
+            save_path = self.save_path
+        torch.save(self.model.state_dict(), save_path)
+
+    def trajectory(self, G_t):
+        self.buffer.trajectory_up(G_t)
+
+    def decision(self, machine_feature, task_feature, D_TM, ninf_mask):
+        machine_feature = machine_feature.to(self.device)
+        task_feature = task_feature.to(self.device)
+        D_TM = D_TM.to(self.device)
+
+        if self.skip:
+            skip_mask = torch.zeros(size=(1, self.machine_num, 1))
+            ninf_mask = torch.cat((skip_mask, ninf_mask), dim=2)
+            ninf_mask = ninf_mask.to(self.device)
+        else:
+            ninf_mask = ninf_mask.to(self.device)
+
+        if self.model.training:
+            probs = \
+                    self.model(machine_feature, task_feature, D_TM, ninf_mask)
+            # [B, M*T]
+            dist = torch.distributions.Categorical(probs)
+            task_selected = dist.sample()
+
+            # [B,]
+            logpa = dist.log_prob(task_selected)
+
+            # radio_pi = (logpa - logpa_old).exp()
+            # radio_pi_clip = radio_pi.clamp(1.0 - self.policy_clip_range,
+            #                                            1.0 + self.policy_clip_range)
+            # [B,]
+            # prob_size = torch.tensor(probs.size(1), device=self.device)
+            # entropie = torch.clip(dist.entropy() / prob_size.log(), min=0, max=1)
+
+            self.buffer_state.machine_feature = machine_feature.detach().clone()
+            self.buffer_state.task_feature = task_feature.detach().clone()
+            self.buffer_state.D_TM = D_TM.detach().clone()
+            self.buffer_state.ninf_mask = ninf_mask.detach().clone()
+            self.buffer_state.logpa = logpa.detach().clone()
+
+            self.buffer.put(copy.deepcopy(self.buffer_state))
+
+            return task_selected.detach().cpu().item()
+
+        else:
+            with torch.no_grad():
+               probs = \
+                        self.model(machine_feature, task_feature, D_TM, ninf_mask)
+            task_selected = probs.argmax(dim=1)
+            return task_selected.detach().cpu().item()
+
+    def optimize_model(self, entropy_loss_weight):
+        exps, G_ts = self.buffer.get_trajectory()
+        G_T = torch.tensor(G_ts, dtype=torch.float32).to(self.device)
+        advantage_t = G_T - G_T.mean()
+        
+        loss_means = []
+        for _ in range(10):
+            n_samples = self.buffer.trajectory_pointer
+            sample = n_samples // 2
+            idxs = np.random.choice(n_samples, sample, replace=False)
+
+            batch_exps = exps[idxs]
+            batch_adv = advantage_t[idxs]
+
+            r_min_sum = []
+            for exp in batch_exps:
+                r_min = []
+                for state in exp:
+                    machine_feature = state.machine_feature.clone()
+                    task_feature = state.task_feature.clone()
+                    D_TM = state.D_TM.clone()
+                    ninf_mask = state.ninf_mask.clone()
+                    
+                    logpa_old = state.logpa.clone()
+                    
+                    machine_feature = machine_feature.to(self.device)
+                    task_feature = task_feature.to(self.device)
+                    D_TM = D_TM.to(self.device)
+                    ninf_mask = ninf_mask.to(self.device)
+                    logpa_old = logpa_old.to(self.device)
+
+                    if self.model.training:
+                        probs = self.model(machine_feature, 
+                                                    task_feature, 
+                                                    D_TM, 
+                                                    ninf_mask)
+                        dist = torch.distributions.Categorical(probs)
+                        task_selected = dist.sample()
+                        logpa = dist.log_prob(task_selected)
+
+                        prob_size = torch.tensor(probs.size(1), device=self.device)
+                        entropie = torch.clip(dist.entropy() / prob_size.log(), min=0, max=1)
+
+                        radio_pi = (logpa - logpa_old).exp()
+                        radio_pi_clip = radio_pi.clamp(1.0 - self.policy_clip_range,
+                                                                1.0 + self.policy_clip_range)
+                        min_radios = torch.min(radio_pi, radio_pi_clip)
+
+                        r_min.append(min_radios.log())
+                r_min = torch.stack(r_min).squeeze()
+                r_min_sum.append(r_min.sum())
+                # print(r_min.sum())
+            r_min_sum = torch.stack(r_min_sum).squeeze()
+            policy_loss = -(batch_adv.detach()*100 + r_min_sum).mean()
+
+            # entropie_loss = -entropies.mean()
+            loss_mean = (self.policy_loss_weight * policy_loss)
+            # loss_mean = (self.policy_loss_weight * policy_loss \
+            #             + entropy_loss_weight * entropie_loss)
+
+            self.optimizer.zero_grad()
+            loss_mean.backward()
+            self.optimizer.step()
+            loss_means.append(loss_mean.detach().cpu().numpy())
+            print(f"policy : {self.policy_loss_weight * policy_loss.detach().cpu().numpy()}")
+            # print(f"entropy : {entropy_loss_weight * entropie_loss.detach().cpu().numpy()}")
+
+        self.buffer.reset()
+        return np.mean(loss_means)
